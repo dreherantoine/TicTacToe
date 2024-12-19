f2a46288c62638345bac6c244e4e4d161ef9abf1
Dreher Antoine <dreher.antoine@gmail.com>
2024-12-19 11:50:40 +0100

    Add RandomPlayerTest

---
+++ TicTacToeTest/RandomPlayerTest.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using FluentAssertions;
+using TicTacToe;
+using TicTacToe.Boards;
+using TicTacToe.Players;
+
+namespace TicTacToeTest
+{
+    public class RandomPlayerTest
+    {
+        [Fact]
+        public void GetNextMove_Default_ReturnsAValidPlayerMove()
+        {
+            // Arrange
+            RandomPlayer randomPlayer = new RandomPlayer(PlayerConstants.PlayerOneIcon);
+
+            // Act
+            Result<PlayerMove> actualMove = randomPlayer.GetNextMove();
+
+            // Assert
+            actualMove.IsSuccess
+                .Should()
+                .BeTrue();
+
+            actualMove.Value.Row
+                .Should()
+                .BeInRange(1, 3);
+
+            actualMove.Value.Column
+                .Should()
+                .BeInRange(1, 3);
+        }
+    }
+}
